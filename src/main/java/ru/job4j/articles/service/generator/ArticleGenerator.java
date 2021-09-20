e9a146573d51ad3599913821d6b2ec3f599b6658
KarnaukhovKirill <kirya2361@gmail.com>
2021-09-20 23:32:18 +0300

    Тестовое задание

--- src/main/java/ru/job4j/articles/service/generator/ArticleGenerator.java
+++ src/main/java/ru/job4j/articles/service/generator/ArticleGenerator.java
@@ -3,8 +3,9 @@ package ru.job4j.articles.service.generator;
 import ru.job4j.articles.model.Article;
 import ru.job4j.articles.model.Word;
 
+import java.lang.ref.SoftReference;
 import java.util.List;
 
 public interface ArticleGenerator {
-    Article generate(List<Word> words);
+    SoftReference<Article> generate(List<Word> words);
 }
